1a9f65930f06732b88024e38d5717ad5ca3f3998
Massimo Manganiello <massimo.manganiello@gmail.com>
2023-08-31 20:31:17 +0200

    Create ConsoleMenuApp.java

---
+++ ConsoleMenuApp.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ConsoleMenuApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("0. Exit");
+            System.out.println("1. Option A");
+            System.out.println("2. Option B");
+            System.out.println("3. Option C");
+            System.out.println("4. Option D");
+            System.out.println("5. Option E");
+            System.out.print("Select an option: ");
+            
+            int choice = scanner.nextInt();
+            
+            switch (choice) {
+                case 0:
+                    System.out.println("Exiting the program.");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                case 1:
+                    System.out.println("You selected Option A.");
+                    break;
+                case 2:
+                    System.out.println("You selected Option B.");
+                    break;
+                case 3:
+                    System.out.println("You selected Option C.");
+                    break;
+                case 4:
+                    System.out.println("You selected Option D.");
+                    break;
+                case 5:
+                    System.out.println("You selected Option E.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+        }
+    }
+}
